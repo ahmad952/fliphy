87bee68c901e9c6ca9aa4ad7a738df0c1e6b370c
Ahmad Abou <ahmed-952@hotmail.com>
2022-10-01 00:17:57 +0200

    Einbinden con skeleton_text, mit hielfe plugin skeleton_text , Definitin von Skeleton Elementen während des Ladevorgangs statfinden

--- lib/widgets/Gif_thumb.dart
+++ lib/widgets/Gif_thumb.dart
@@ -1,5 +1,6 @@
 import 'package:fliphy/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 import 'package:video_player/video_player.dart';
 
 class GifThaumb extends StatefulWidget{
@@ -14,15 +15,20 @@ class GifThaumb extends StatefulWidget{
 class GifThumbState extends State<GifThaumb> {
 
   late VideoPlayerController _controller;
+  // derfinieren neue zustand
+  bool _istloding= false;
 
   @override
   void initState() {
 
     super.initState();
+    _istloding =true;
     _controller =VideoPlayerController.network(widget.Url)..initialize().then((_){});
     setState(() {
-
+      // sobald das video initialisert wurde
+_istloding=false;
     });
+
     //lazy loding innerhalb der Listview
     //mehrer viedos gleich zeitich
     _controller.setVolume(0);
@@ -35,6 +41,15 @@ class GifThumbState extends State<GifThaumb> {
   }
   @override
   Widget build(BuildContext context) {
+    if(_istloding ==true){
+      return SkeletonAnimation(child: Container(
+        // die  gesamte box ausfüllen muss alsu keine schwrze meher
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(color: Colors.black45),
+// also lader animation auftreten
+      )
+      );
+    }
     return VideoPlayer(_controller);
   }
 
